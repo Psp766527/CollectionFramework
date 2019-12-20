d948d5259737657eb6e58e7752c75a650b04b8ea
Psp766527 <55358571+Psp766527@users.noreply.github.com>
2019-12-21 00:03:00 +0530

    Arraylist added

--- ArrayListExmp.java
+++ ArrayListExmp.java
@@ -1,21 +1,12 @@
-package com.nt.servlet;
-
-import javax.servlet.*;
-import java.io.*;
 import java.util.*;
 
-public class DateServlet extends GenericServlet
+public class ArrayListExmp 
 {
-	public void service(ServletRequest req,ServletResponse res)throws ServletException,IOException
+	public static void main(String... args)
 	{
-		//set response content type
-			res.setContentType("text/html");
-		//get printWritter object
-		PrintWriter pw=res.getWriter();
-		// write response
-		Date d=new Date();
-		pw.println("<b><i><center>Date and Time is "+d+"  </b></i></center>");
-		//close stream
-		pw.close();
-	}//service(-,-)
+		List s = new ArrayList();
+		s.add(10);
+		s.add(20);
+		System.out.println(s);
+	}
 }//class
